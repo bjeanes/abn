cbd867cc75dc534b72bf36b5321dbb4e52c971b6
Bodaniel Jeanes <me@bjeanes.com>
2008-08-14 15:56:04 +1000

    refactoring and abstracting the drivers

--- spec/abn_api_spec.rb
+++ spec/abn_api_spec.rb
@@ -2,7 +2,11 @@ $LOAD_PATH << File.dirname(__FILE__) + "/../lib"
 
 require 'spec'
 require 'abn'
-require 'api/defaultDriver'
+require 'api/api'
+
+# API Key for Bodaniel Jeanes (me:at:bjeanes:dot:com)
+#   used here for *testing* purposes only
+api_key = "055a1100-a84d-4064-84d7-bce46f7b80c8"
 
 describe "The ABN API" do
   it "should connect to server" do
@@ -11,7 +15,7 @@ describe "The ABN API" do
   
   it "should get a result" do
     api = ABRXMLSearchSoap.new
-    param = ABRSearchByABN.new("12042168743", "N", "055a1100-a84d-4064-84d7-bce46f7b80c8")
+    param = ABRSearchByABN.new("12042168743", "N", api_key)
     api.aBRSearchByABN(param)
   end
   
