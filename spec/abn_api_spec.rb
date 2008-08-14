155ed19a106babb18197eef7f94d3e1a1f1c6f10
Bodaniel Jeanes <me@bjeanes.com>
2008-08-14 15:33:11 +1000

    api specs will go here

---
+++ spec/abn_api_spec.rb
@@ -0,0 +1,18 @@
+$LOAD_PATH << File.dirname(__FILE__) + "/../lib"
+
+require 'spec'
+require 'abn'
+require 'api2/defaultDriver'
+
+describe "The ABN API" do
+  it "should connect to server" do
+    api = ABRXMLSearchSoap.new
+  end
+  
+  it "should get a result" do
+    api = ABRXMLSearchSoap.new
+    param = ABRSearchByABN.new("12042168743", "N", "055a1100-a84d-4064-84d7-bce46f7b80c8")
+    api.aBRSearchByABN(param)
+  end
+  
+end
